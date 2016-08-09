f00e11eeea778e44b5e70b780c8321b333656ebd
Dev Sanghani <dev_sanghani@yahoo.com>
2016-08-09 19:27:04 +0530

    Add methods to header

--- objc/KVDatabaseDataQueue.h
+++ objc/KVDatabaseDataQueue.h
@@ -13,4 +13,13 @@
 @property (nonatomic, strong) NSData *(^encoderBlock)(id anObject);
 @property (nonatomic, strong) id(^decoderBlock)(NSData *data);
 
+- (instancetype)initDatabaseQueueWithPath:(NSString *)path;
+
+- (id)objectForKey:(NSString *)aKey;
+- (void)setObject:(id)obj forKey:(NSString *)aKey;
+- (void)removeObjectForKey:(NSString *)aKey;
+
+- (void)purgeCache;
+- (void)close;
+
 @end
